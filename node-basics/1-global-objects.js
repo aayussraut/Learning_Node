7b60ad2c673c2be9abe51c385f0ac110c3f9f060
iush <aayussraut.ar@gmail.com>
2022-05-08 21:50:59 +0545

    Global Object in Node.js

---
+++ node-basics/1-global-objects.js
@@ -0,0 +1,17 @@
+// function sayHello(name){
+//     console.log("Hello "+name);
+// }
+
+sayHello=(name)=>{
+    console.log("Hello "+name);
+}
+
+sayHello("aayush");
+
+// console.log(); global object can be used anywhere
+
+setTimeout() // used to call a function after a delay
+clearTimeout() //stop
+
+setInterval() //repeatatively call a function after a certain delay
+clearInterval() //stop
