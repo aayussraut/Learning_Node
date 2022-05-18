a3410a6b898a4dd5df5ecaff7027460b0a200889
aayussraut <aayussraut.ar@gmail.com>
2022-05-18 21:25:22 +0545

    index.js

--- vidly-mongodb/index.js
+++ vidly-mongodb/index.js
@@ -1,25 +1,17 @@
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
-const genre=require("./routes/genre");
-const customer=require("./routes/customer-api");
-const movie=require("./routes/movie-api");
-const rental=require("./routes/rental-api");
-const user=require("./routes/user-api");
-const auth=require("./routes/auth");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const config=require("config");
 
-mongoose.connect('mongodb://localhost/vidly') //connection to mongodb database
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err => console.error("Could not connect to database", err)));
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+// mongoose
+//   .connect("mongodb://localhost/vidly") //connection to mongodb database
+//   .then(() => console.log("Connected to MongoDB"));
 
-app.use(express.json());
-app.use("/api/genres",genre);
-app.use("/api/customers",customer);
-app.use("/api/movies",movie);
-app.use("/api/rental",rental);
-app.use("/api/users",user);
-app.use("/api/auth",auth);
-app.listen(3000,()=>{
-    console.log('Listening to port 3000......');
-})
 
+app.listen(3000, () => {
+  console.log("Listening to port 3000......");
+});
